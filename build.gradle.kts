49d705add15568735ff54775e019af0676cbbe0f
Guemiza <nesrine.f@live.com>
2024-05-06 00:25:42 +0100

    exam devops

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    kotlin("jvm") version "1.5.21"
     application
-    kotlin("jvm") version "1.3.21"
     id("com.github.johnrengelman.shadow") version "4.0.4"
+    id ("com.github.ben-manes.versions") version "0.39.0"
+   
 }
 
 application {
+    // Ensure the main class name is correctly referenced
     mainClassName = "MainKt"
 }
 
-dependencies {
-    compile(kotlin("stdlib"))
+repositories {
+    // Replace jcenter() with mavenCentral()
+    mavenCentral()
 }
 
-repositories {
-    jcenter()
+dependencies {
+    // Update to use the 'implementation' keyword which is preferred over 'compile'
+    implementation(kotlin("stdlib-jdk8"))
+
+    // Add Ktor dependencies
+    implementation("io.ktor:ktor-server-netty:1.6.7")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("io.ktor:ktor-server-core:1.6.7")
+    implementation("io.ktor:ktor-http-jvm:1.6.7")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Jar> {
+    // Ensure the application can find the main class when running the shadow JAR
+    manifest {
+        attributes("Main-Class" to application.mainClassName)
+    }
+
+    // Configure the shadow JAR plugin to include all dependencies in the JAR
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
-fun main(args: Array<String>) {
-    println("Hello, world!")
-    var sum = 0;
-    args.forEach {
-        sum += it.toInt()
-    }
-    println("Sum:  $sum")
+fun main() {
+    embeddedServer(Netty, host = "0.0.0.0", port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Plain)
+            }
+            get("/sum") {
+                val numbers = call.request.queryParameters.getAll("num")?.map { it.toInt() } ?: listOf()
+                val sum = numbers.sum()
+                call.respondText("Sum: $sum", ContentType.Text.Plain)
+            }
+        }
+    }.start(wait = true)
 }
+
